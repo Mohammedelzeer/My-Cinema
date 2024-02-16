f889f61bb77013c5da1cc4d0b53ac3aabfc9fc9b
Mohammed Mamdouh <mohammedmamdouh256@gmail.com>
2024-02-16 20:42:06 +0200

    Initial Commit

---
+++ My Cinema/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  My Cinema
+//
+//  Created by Mohammed on 16/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ My Cinema/My_CinemaApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_CinemaApp.swift
+//  My Cinema
+//
+//  Created by Mohammed on 16/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct My_CinemaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
